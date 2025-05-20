f366bdd3e98c01b59c8eca35288ed0a67f55ec9c
Victor Aires <victorairesjp@gmail.com>
2025-05-20 08:15:55 -0300

    feat páginas temas e css

--- app.py
+++ app.py
@@ -6,4 +6,35 @@ app = Flask(__name__)
 def ola():
     return render_template('index.html')
 
-app.run()
+
+@app.route('/sobre-equipe')
+def sobreEquipe():
+    return render_template('sobre-equipe.html')
+
+
+@app.route('/variaveis')
+def variaveis():
+    return render_template('variaveis.html')
+
+@app.route('/tipos-dados')
+def tipos_dados():
+    return render_template('tipos-dados.html')
+
+@app.route('/estruturas-controle')
+def estruturas_controle():
+    return render_template('estruturas-controle.html')
+
+@app.route('/funcoes')
+def funcoes():
+    return render_template('funcoes.html')
+
+@app.route('/listas')
+def listas():
+    return render_template('listas.html')
+
+@app.route('/dicionarios')
+def dicionarios():
+    return render_template('dicionarios.html')
+
+
+app.run(debug=True)
